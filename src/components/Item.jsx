99b220f8867b3b240ad15d860f48fef6ef414043
StephaneRavet <ravet.st@gmail.com>
2023-08-02 13:47:40 +0200

    front

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Item({ item }) {
+  const quantity = item?.CharacterItem?.quantity && ('x' + item?.CharacterItem?.quantity)
   return (
     <span style={{ marginRight: 4 }}>
       <span className="badge badge-success">
         {item?.name}
-      </span>x{item?.CharacterItem?.quantity}
+      </span>
+      {quantity}
     </span>
   )
 }
