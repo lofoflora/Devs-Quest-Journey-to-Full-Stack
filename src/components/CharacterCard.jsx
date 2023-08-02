9616935119411ede6a3e1b9d454dc45bcbb3d5f2
StephaneRavet <ravet.st@gmail.com>
2023-08-02 15:45:12 +0200

    front

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import Item from './Item';
+import { useState, useEffect } from 'react';
 
 const CharacterCard = ({ character = {} }) => {
+  const [skills, setSkills] = useState([])
+  const [characterSkills, setCharacterSkills] = useState([])
+  useEffect(() => {
+    fetch(`/api/skills`)
+      .then(res => res.json())
+      .then(skills => setSkills(skills))
+  }, [])
+  useEffect(() => {
+    character.skills = character.skills ?? []
+    const allSkills = [...new Set([...skills, ...character.skills ?? []])]
+    setCharacterSkills(allSkills.map(skill => {
+      return {
+        name: skill,
+        owned: character.skills.includes(skill),
+        special: skills.includes(skill),
+      }
+    }))
+  }, [skills, character])
   return (
     <div className="card">
       <img src={character.avatar} className="card-img-top" alt={character.name} />
@@ -10,6 +30,12 @@ const CharacterCard = ({ character = {} }) => {
         <p className="card-text">Niveau : {character.level}</p>
         <p className="card-text">XP : {character.experience}</p>
         <p className="card-text">Argent : {character.money}</p>
+      </div>
+      <div className="card-body">
+        <div className="card-title">Compétences</div>
+        <div className="card-text">
+          {skills.map((item, index) => <Item item={item} key={index} />)}
+        </div>
       </div>
     </div>
   );
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import classNames from 'classnames'
 
 function Item({ item }) {
   const quantity = item?.CharacterItem?.quantity && ('x' + item?.CharacterItem?.quantity)
   return (
     <span style={{ marginRight: 4 }}>
-      <span className="badge badge-success">
+      <span className={classNames('badge badge-secondary', {
+        'badge-success': quantity,
+        'badge-primary': item.owned,
+        'badge-warning': item.special,
+      })}>
         {item?.name}
       </span>
       {quantity}
