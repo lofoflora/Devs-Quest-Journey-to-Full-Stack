00262784a8b6ea0fb04a1cb4d586ac74789b0c62
HiX <133093398+HiixX@users.noreply.github.com>
2023-08-02 19:10:44 +0200

    Fin ModObjets

--- server/routes/item.routes.js
+++ server/routes/item.routes.js
@@ -1,9 +1,30 @@
 import { Router } from "express";
-import { Item } from "../models/index.js";
+import { Character, Item } from "../models/index.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
+  try {
+    const characters = await Character.findAll();
+    res.json(characters);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, specialization } = req.body;
+    const newCharacter = await Character.create({ name, specialization });
+    res.json(newCharacter);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/item", async (req, res) => {
   try {
     const items = await Item.findAll();
     res.json(items);
@@ -13,17 +34,11 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/item", async (req, res) => {
   try {
-    const { name, description, bonus, malus, price } = req.body;
-    const newItem = await Item.create({
-      name,
-      description,
-      bonus,
-      malus,
-      price,
-    });
-    res.json(newItem);
+    const { name, bonus, malus, price } = req.body;
+    const NewItem = await Item.create({ name, bonus, malus, price });
+    res.json(NewItem);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: err.message });
