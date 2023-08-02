5d036df0af4f97f10e2c242d983a37c5e6622ecf
HiX <133093398+HiixX@users.noreply.github.com>
2023-08-02 14:36:29 +0200

    Deb Item

--- server/models/Character.model.js
+++ server/models/Character.model.js
@@ -24,9 +24,41 @@ const Character = database.define('Character', {
     type: DataTypes.INTEGER,
     defaultValue: 0,
   },
-  //skills: ?, // TODO
-  //items: ?, //TODO
   money: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  });
+  //skills: ?, // TODO
+
+
+
+
+  const Item = database.define('Item', {
+
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    bonus: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    malus: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+  price: {
     type: DataTypes.INTEGER,
     defaultValue: 0,
   },
